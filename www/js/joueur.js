d8889570b7056116de2614f5c8eee731ab7ee631
Pierre-Adrien Ghiringhelli <ghiringhelli.pierreadrien@gmail.com>
2017-05-01 17:18:12 +0200

    radar chart

--- www/js/joueur.js
+++ www/js/joueur.js
@@ -29,13 +29,65 @@ angular.module('novemstat').factory('joueursService', function($http) {
 		});
 	}
 
+	service.getScores = function(){
+		return $http({
+			method: 'GET',
+			url: '/api-proxy' + '/scores'
+		}).then(function(res) {
+			return res.data;
+		});
+	}
+
 	return service;
 });
 
-angular.module('novemstat').controller('joueurListCtrl', function(joueursService) {
+angular.module('novemstat').controller('joueurListCtrl', function(joueursService, eventsService) {
 	var ctrl = this;
 	joueursService.getJoueurs().then(function(data) {
 		ctrl.joueurs = data;
+		joueursService.getScores().then(function(scores){
+			eventsService.getEvents().then(function(evenements){
+				angular.forEach(ctrl.joueurs, function(joueur){
+					angular.forEach(scores, function(score){
+						if(score.joueur_id == joueur._id){
+							joueur.score = score;
+							joueur.chart = {
+								type: 'radar',
+								data: {
+									labels: ["Business", "Communication", "Coding", "Management", "Marketing", "Multimedia"],
+									datasets: [
+										{
+											label: "Tes compétences",
+											backgroundColor: "rgba(61,103,193,0.3)",
+											borderColor: "rgba(61,103,193,1)",
+											pointBackgroundColor: "rgba(61,103,193,1)",
+											pointBorderColor: "#fff",
+											pointHoverBackgroundColor: "#fff",
+											pointHoverBorderColor: "rgba(61,103,193,1)",
+											data: [Math.random()*90+10, Math.random()*90+10, Math.random()*90+10, Math.random()*90+10, Math.random()*90+10, Math.random()*90+10]
+										}
+									]
+								},
+								options: {
+									scale: {
+										ticks: {
+											beginAtZero: true
+										}
+									}
+								}
+							}
+							var place = document.getElementById(joueur._id);
+							var radarChart = new Chart(place, joueur.chart);
+						}
+					});
+					angular.forEach(evenements, function(evenement){
+						if(joueur.evenement_id == evenement._id){
+							joueur.evenement = evenement;
+						}
+					});
+				});
+			});	
+		});
 		console.log(data);
 	});
 });
